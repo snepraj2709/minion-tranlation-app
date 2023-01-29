b6fef8829dc2b6e516fb392c6a65bfc02a55e407
snepraj2709 <snehaprajapatijnv15@gmail.com>
2023-01-29 23:32:23 +0530

    Minion tranlastion API done in th GUI

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 //input as click event on the button
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(txtInput);
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-//process that the click happened
-btnTranslate.addEventListener("click", clickhandler);
-
-//output based on that Click input and the above processing
-function clickhandler() {
-  console.log("Clicked");
-  console.log("input", txtInput.value);
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
 }
+
+function errorhandler(error) {
+  console.log("error occured", error);
+  alert("Something wrong with server! try agaian after some time");
+}
+
+function clickhandler() {
+  // outputDiv.innerText = "random text" + txtInput.value;
+
+  // taking input
+  var inputText = txtInput.value;
+
+  fetch(getTranslationURL(inputText))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+      //   console.log(json.contents.translated);
+    })
+    .catch(errorhandler);
+}
+//process the click
+btnTranslate.addEventListener("click", clickhandler);
