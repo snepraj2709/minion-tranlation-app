883cadd092f292adeed689abdd28e609cb27dc56
snepraj2709 <snehaprajapatijnv15@gmail.com>
2023-01-29 19:31:48 +0530

    Added script to alert the output and a button

---
+++ app.js
@@ -0,0 +1,8 @@
+//taking input from user
+var username = prompt("What's your name?");
+
+//processing hte input for some output
+var welcomeMessage = "this script works!" + username;
+
+//output
+alert(welcomeMessage);
