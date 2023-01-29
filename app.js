5c8757b250f1e58139312cffe6d4d1ca80f08de2
snepraj2709 <snehaprajapatijnv15@gmail.com>
2023-01-29 20:08:46 +0530

    Taking input and console the value using DOM

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+//input as click event on the button
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 
-btnTranslate.addEventListener("click", function clickEventHandler() {
-  console.log("clicked");
-});
+console.log(txtInput);
+
+//process that the click happened
+btnTranslate.addEventListener("click", clickhandler);
+
+//output based on that Click input and the above processing
+function clickhandler() {
+  console.log("Clicked");
+  console.log("input", txtInput.value);
+}
